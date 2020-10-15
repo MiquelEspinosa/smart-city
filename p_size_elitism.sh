de1a1f00abc06e043fcee23ee5c6cd3f25057095
MiquelEspinosa <100383404@alumnos.uc3m.es>
2020-10-15 18:13:14 +0100

    folder

--- p_size_elitism.sh
+++ p_size_elitism.sh
@@ -1,19 +1,19 @@
-python3 main.py -f "./experiments/elitism/p20_1" -p 20 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p20_2" -p 20 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p20_3" -p 20 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p20_1" -p 20 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p20_2" -p 20 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p20_3" -p 20 -t 0.05 -m 0.05 -e True
 
-python3 main.py -f "./experiments/elitism/p40_1" -p 40 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p40_2" -p 40 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p40_3" -p 40 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p40_1" -p 40 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p40_2" -p 40 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p40_3" -p 40 -t 0.05 -m 0.05 -e True
 
-python3 main.py -f "./experiments/elitism/p60_1" -p 60 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p60_2" -p 60 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p60_3" -p 60 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p60_1" -p 60 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p60_2" -p 60 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p60_3" -p 60 -t 0.05 -m 0.05 -e True
 
-python3 main.py -f "./experiments/elitism/p80_1" -p 80 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p80_2" -p 80 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p80_3" -p 80 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p80_1" -p 80 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p80_2" -p 80 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p80_3" -p 80 -t 0.05 -m 0.05 -e True
 
-python3 main.py -f "./experiments/elitism/p100_1" -p 100 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p100_2" -p 100 -t 0.05 -m 0.05 -e True
-python3 main.py -f "./experiments/elitism/p100_3" -p 100 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p100_1" -p 100 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p100_2" -p 100 -t 0.05 -m 0.05 -e True
+python3 main.py -f "./experiments/psize/elitism/p100_3" -p 100 -t 0.05 -m 0.05 -e True
